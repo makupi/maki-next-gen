3acb0b524713892fe5e01fdc3d15b74829af4d9c
makubob <makupi@icloud.com>
2020-05-13 23:30:41 +0200

    Adds proper ForeignKeys

--- maki/database/models/users.py
+++ maki/database/models/users.py
@@ -6,7 +6,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.BIGINT)
-    guild_id = db.Column(db.BIGINT)
+    guild_id = db.Column(db.BIGINT, db.ForeignKey("guilds.id"))
     birthday = db.Column(db.Date)
 
     _guild_user_uniq = db.UniqueConstraint("user_id", "guild_id", name="guild_user")
