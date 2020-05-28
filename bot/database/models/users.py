b872e7a2fe7c99575768dffc2bf8996844c39dbb
makubob <makupi@icloud.com>
2020-05-29 00:02:51 +0200

    Updates Users table for Economy

--- bot/database/models/users.py
+++ bot/database/models/users.py
@@ -8,5 +8,8 @@ class User(db.Model):
     user_id = db.Column(db.BIGINT)
     guild_id = db.Column(db.BIGINT, db.ForeignKey("guilds.id"))
     birthday = db.Column(db.Date)
+    money = db.Column(db.Numeric(precision=4), default=0)
+    xp = db.Column(db.Integer, default=0)
+    level = db.Column(db.Integer, default=0)
 
     _guild_user_uniq = db.UniqueConstraint("user_id", "guild_id", name="guild_user")
