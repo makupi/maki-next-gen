e3f76fd58783f10083d4e3b2ba9f006dbd8a28a7
makubob <makupi@icloud.com>
2020-11-05 22:31:02 +0100

    Add database naming conventions
    - delete old revisions
    - generate new revision

--- bot/database/__init__.py
+++ bot/database/__init__.py
@@ -1,7 +1,16 @@
 from gino import Gino
 from bot.utils import config
 
-db = Gino()
+
+naming_convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+db = Gino(naming_convention=naming_convention)
 
 # import models so Gino registers them
 import bot.database.models as models  # isort:skip
